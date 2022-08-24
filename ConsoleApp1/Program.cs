dff0db39bd55bd07509ae663b2b678b6ed1c50f9
ashok-02 <ashok02ash@gmail.com>
2022-08-24 09:36:33 +0530

    yy

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,7 +7,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Today is Thrusday");
+            Console.WriteLine("Today is Thrusday,,,,,");
         }
     }
 }
